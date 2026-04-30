79c505aa14fdb7360443827f0b7aadc340e907f7
Rodrigo <rodrigodeifelt@gmail.com>
2026-04-29 22:24:22 -0300

    Automação do Contato

---
+++ cypress/e2e/contato.cy.js
@@ -0,0 +1,54 @@
+describe('Funcionalidade: Contato', () => {
+
+  beforeEach(() => {
+    cy.visit('index.html')
+  });
+
+  it('Deve preencher formulário de contato com sucesso', () => {
+    cy.get('[name="name"]').type('Rodrigo Deifelt')
+    cy.get('[name="email"]').type("rodrigo@teste.com")
+    cy.get('[name="subject"]').select("Parcerias")
+    cy.get('[name="message"]').type('Mensagem de teste')
+    cy.get('#btn-submit').click()
+    //Resultado esperado
+    cy.contains('Contato enviado com sucesso!').should('exist')
+  });
+
+  it('Deve validar mensagem de erro ao enviar sem preencher nome', () => {
+    cy.get('#name').clear()
+    cy.get('[name="email"]').type("rodrigo@teste.com")
+    cy.get('[name="subject"]').select("Parcerias")
+    cy.get('[name="message"]').type('Mensagem de teste')
+    cy.get('#btn-submit').click()
+    cy.get('#alert-container').should('contain', 'Por favor, preencha o campo Nome.')
+  });
+
+  it('Deve validar mensagem de erro ao enviar sem preencher email', () => {
+    cy.get('[name="name"]').type('Rodrigo Deifelt')
+    cy.get('[name="email"]').clear()
+    cy.get('[name="subject"]').select("Parcerias")
+    cy.get('[name="message"]').type('Mensagem de teste')
+    cy.get('#btn-submit').click()
+    cy.get('#alert-container').should('contain', 'Por favor, preencha o campo E-mail.')
+  });
+
+  it('Deve validar mensagem de erro ao enviar sem preencher o assunto', () => {
+    cy.get('[name="name"]').type('Rodrigo Deifelt')
+    cy.get('[name="email"]').type("rodrigo@teste.com")
+    //cy.get('[name="subject"]').select('Parcerias')
+    cy.get('[name="message"]').type('Mensagem de teste')
+    cy.get('#btn-submit').click()
+    //Resultado esperado
+    cy.get('#alert-container').should('contain', 'Por favor, selecione o Assunto.')
+  });
+
+  it('Deve validar mensagem de erro ao enviar sem preencher a mensagem', () => {
+    cy.get('[name="name"]').type('Rodrigo Deifelt')
+    cy.get('[name="email"]').type("rodrigo@teste.com")
+    cy.get('[name="subject"]').select('Parcerias')
+    //cy.get('[name="message"]').type('Mensagem de teste')
+    cy.get('#btn-submit').click()
+    //Resultado esperado
+    cy.get('#alert-container').should('contain', 'Por favor, escreva sua Mensagem.')
+  });
+});
