ff0a1e3ed646ac2e654435b921c8367e1caf9c03
Rodrigo <rodrigodeifelt@gmail.com>
2026-04-30 10:24:37 -0300

    Automação da lista de catálogo

---
+++ cypress/e2e/cadastro.cy.js
@@ -0,0 +1,39 @@
+/// <reference types="cypress"/>
+import { faker } from '@faker-js/faker';
+
+describe('Funcionalidade: Cadastro no Hub de Leitura', () => {
+
+    beforeEach(() => {
+        cy.visit('register.html')
+    });
+
+    it('Deve fazer cadastro com sucesso, usando função JS', () => {
+        let email = `teste${Date.now()}@teste.com`
+        cy.get('#name').type('Rodrigo')
+        cy.get('#email').type(email)
+        cy.get('#phone').type('5111111111')
+        cy.get('#password').type('Teste@123')
+        cy.get('#confirm-password').type('Teste@123')
+        cy.get('#terms-agreement').check()
+        cy.get('#register-btn').click()
+        //Resultado esperado
+        cy.url().should('include', 'dashboard')
+
+    });
+
+    it.only('Deve fazer cadastro com sucesso usando Faker', () => {
+        let email = faker.internet.email()
+        let nome = faker.person.fullName()
+        cy.get('#name').type(nome)
+        cy.get('#email').type(email)
+        cy.get('#phone').type('5111111111')
+        cy.get('#password').type('Teste@123')
+        cy.get('#confirm-password').type('Teste@123')
+        cy.get('#terms-agreement').check()
+        cy.get('#register-btn').click()
+        //Resultado esperado
+        cy.url().should('include', 'dashboard')
+        cy.get('#user-name').should('contain', nome)
+
+    });
+});
